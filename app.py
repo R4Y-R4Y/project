c20cd8ecc1ba36fe674370c6eb51442b33283b6a
R4Y <fredrobinson697@hotmail.com>
2022-11-16 11:42:53 +0100

    added current session changes

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flask import Flask, request
+from db import stores
+import uuid
+import sys
 app = Flask(__name__)
-stores = [
-    {
-        "name": "Big Store",
-        "id": 0,
-        "items": [
-            {
-                "id":1,
-                "name": "milk",
-                "price": 0.69
-            },
-        ]
-    }
-]
+
 
 @app.get("/stores")
 
 def get_stores():
     return{"stores": stores}
 
+@app.get("/stores/<int:id>")
+
+def get_store(id):
+    for x in stores:
+        # if you want to print log in the terminal try this
+        print(x, file=sys.stderr)
+        if(x['id'] == id):
+            return({"store":x})
+    return {"Message":"Store with id "+ str(id) +" not Found"},404
+
 @app.post("/stores")
 
 def create_store():
     request_data = request.get_json()
-    new_store = {"name": request_data["name"],"id":len(stores), "items":[]}
+    new_store = {"name": request_data["name"],"id":uuid.uuid4().hex, "items":[]}
     stores.append(new_store)
     return new_store
 
---
+++ db.py
@@ -0,0 +1,6 @@
+stores = []
+# {
+# 	"id": "aad22fe3ca4b47df9a261249829b723e",
+# 	"items": [],
+# 	"name": "funny store"
+# }
